45208efc4bd5b4975c067e26a666c16796960da7
Zesiria <78583534+Zesiria@users.noreply.github.com>
2022-09-01 16:07:16 +0700

    Initial Commit

---
+++ src/main/java/th/ac/ku/restaurant/RestaurantWebApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWebApplication.class, args);
+	}
+
+}
